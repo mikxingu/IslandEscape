67d9cb63c596e451267bc78f5c828f974557fda9
Michel Alves <michelalvs@yahoo.com.br>
2020-11-17 23:07:19 -0300

    Initial project commit

---
+++ Assets/CharacterController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] Transform targetTransform = null;
+
+    NavMeshAgent currentAgent;
+	private void Start()
+	{
+		currentAgent = GetComponent<NavMeshAgent>();
+	}
+
+	void Update()
+    {
+        if (targetTransform != null)
+		{
+			currentAgent.SetDestination(targetTransform.position);
+		}
+		if (Input.GetKeyDown(KeyCode.K))
+		{
+			targetTransform = null;
+		}
+    }
+}

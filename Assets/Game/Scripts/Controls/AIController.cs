4a7e93418b3e9bdc34a45697715d4971a64e4b19
Michel Alves <michelalvs@yahoo.com.br>
2020-11-24 19:05:20 -0300

    Implemented enemy combat radius.

---
+++ Assets/Game/Scripts/Controls/AIController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+
+/* == TODO ==
+ * Implement Line of Sight and Combat Radius:
+ * Combat Radius: The radius of enemy awareness. If you attack one particular enemy, all enemies 
+ * nearby will notice and will automatically follow will. The Condition will be like 
+ * if one particular enemy has the aggro on you and the other enemies are in this enemy combat range, they will follow will even if you're not on their combat radius.
+ * 
+ * Line of Sight: The enemies only look forward, so, they will only target you if you cross their line of sight.
+ * This approach will give players the potential to sneak behind enemies to avoid combat, especially on situations where they are outnumbered.
+ * 
+ */
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float combatRadius = 5f;
+
+		private void Update()
+		{
+			GameObject player = GameObject.FindWithTag("Player");
+			bool isInChaseRadius = Vector3.Distance(transform.position, player.transform.position) < combatRadius;
+
+			if (isInChaseRadius)
+			{
+				print(gameObject.name + " will chase the player");
+			}
+		}
+
+		private void OnDrawGizmosSelected()
+		{
+			Gizmos.color = Color.blue;
+			Gizmos.DrawWireSphere(transform.position, combatRadius);
+		}
+
+
+		/* THIS WILL BE USED AS LINE OF SIGHT IN THE FUTURE 
+		void OnDrawGizmosSelected()
+		{
+			Gizmos.color = Color.cyan;
+			Gizmos.DrawRay(transform.position, (Vector3.forward * chaseRadius));
+		}*/
+	}
+}
+
+

8003e6aba6594dadcfc4e2d0072cfe0b41409618
Michel Alves <michelalvs@yahoo.com.br>
2020-12-02 00:09:12 -0300

    Configured Enable and Disable Controls on Cinematics

---
+++ Assets/Game/Scripts/Cinematics/CinematicsControlRemover.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using RPG.Control;
+
+namespace RPG.Cinematics
+{
+	public class CinematicsControlRemover : MonoBehaviour
+	{
+		[SerializeField] PlayerController playerController;
+		private void Start()
+		{
+			GetComponent<PlayableDirector>().played += DisableControls;
+			GetComponent<PlayableDirector>().stopped += EnableControls;
+		}
+		void EnableControls(PlayableDirector playableDirector)
+		{
+			print("Enabled Controls");
+			playerController.enabled = true;
+			
+		}
+
+		void DisableControls(PlayableDirector playableDirector)
+		{
+			print("Disabled Controls");
+			playerController.enabled = false;
+		}
+	}
+}
+
+

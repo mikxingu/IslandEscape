59153ab4e48c29375616c96edaa8cac9fd2be3eb
Mike Alves <michelalvs@gmail.com>
2021-01-28 02:36:36 -0300

    First moment tweak (#7)
    First moment tweak finished

---
+++ Assets/Game/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Playables;
+using RPG.Movement;
+
+namespace RPG.Cinematics
+{
+	
+    public class CinematicTrigger : MonoBehaviour
+    {
+		bool hasPlayed = false;
+		CharacterMover playerMover;
+		private void OnTriggerEnter(Collider other)
+		{
+			if (!hasPlayed && other.gameObject.tag == "Player")
+			{
+				playerMover = other.GetComponent<CharacterMover>();
+				playerMover.MoveToPoint(new Vector3(-16, 0, 17));
+
+				//GetComponent<PlayableDirector>().enabled = true;
+				GetComponent<PlayableDirector>().Play();
+				hasPlayed = true;
+			}
+		}
+	}
+}
---
+++ Assets/Game/Scripts/Cinematics/CinematicsControlRemover.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using RPG.Control;
+
+namespace RPG.Cinematics
+{
+	public class CinematicsControlRemover : MonoBehaviour
+	{
+		[SerializeField] PlayerController playerController;
+
+		PlayableDirector currentDirector;
+
+		private void Start()
+		{
+			currentDirector = GetComponent<PlayableDirector>();
+			currentDirector.stopped += EnableControls;
+			currentDirector.played += DisableControls;
+			
+		}
+		void EnableControls(PlayableDirector playableDirector)
+		{
+			print("Enabled Controls");
+			playerController.enabled = true;
+		}
+
+		void DisableControls(PlayableDirector playableDirector)
+		{
+			print("Disabled Controls");
+			playerController.enabled = false;
+		}
+
+		void Update()
+		{
+			/*
+			print(currentDirector.time);
+			if (currentDirector.time >= 16){
+				currentDirector.enabled = false;
+			}*/
+		
+		}
+	}
+}
+
+
